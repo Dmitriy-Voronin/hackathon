aacbd9777f6075d7aed8ac732fb6ade9e6e3eec7
Dmitry Voronin <voronin2709@gmail.com>
2023-11-25 23:58:13 +0300

    add transfers

--- src/pages/Dashboard/components/Content/Content.tsx
+++ src/pages/Dashboard/components/Content/Content.tsx
@@ -1,6 +1,26 @@
 import { DashboardHeader } from './components/DashboardHeader'
 import { Sessions } from './components/Sessions'
+import { Transfers } from './components/Transfers'
 import styles from './content.module.css'
+
+const list = [
+    {
+        city: 'Москва',
+        value: 10000
+    },
+    {
+        city: 'Москва',
+        value: 45000
+    },
+    {
+        city: 'Париж',
+        value: 5000
+    },
+    {
+        city: 'Тель-Авив',
+        value: 15000
+    },
+]
 
 export const Content = () => {
     return (
@@ -9,8 +29,8 @@ export const Content = () => {
             <main className={styles.main}>
                 <section className={styles.info}>
                     <Sessions dates='1-6 ноября, 2023'/>
-                    {/* <Transfers />
-                    <Visits /> */}
+                    <Transfers list={list}/>
+                    {/* <Visits /> */}
                 </section>
             </main>
         </div>
--- src/pages/Dashboard/components/Content/components/Transfers/Transfers.tsx
+++ src/pages/Dashboard/components/Content/components/Transfers/Transfers.tsx
@@ -1,12 +1,29 @@
 import styles from './transfers.module.css'
 
-type TTransfers = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> 
+type TTransfers = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> & {
+    list: {
+        city: string
+        value: number
+    }[]
+}
 
 
-export const Transfers: React.FC<TTransfers> = ({ ...props }) => {
+export const Transfers: React.FC<TTransfers> = ({ list, ...props }) => {
     return (
         <div className={styles.transfers__container} {...props}>
-
+            <h2 className={styles.transfers__title}>Сеансы по устройствам</h2>
+            <div className={styles.descr}>
+                <span className={styles.descr_item}>Если перевод показался вам подозрительным</span>
+                <span className={styles.descr_item}>Не одобряйте его!</span>
+            </div>
+            <ul className={styles.list}>
+                {list.map((item) => (
+                    <li className={styles.list__item}>
+                            <span className={styles.list__item_city}>{item.city}</span>
+                            <span className={styles.list__item_value}>{item.value} руб</span>
+                    </li>
+                ))}
+            </ul>
         </div>
 
     )
